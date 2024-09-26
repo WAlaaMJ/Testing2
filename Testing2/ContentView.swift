5b51f3b815f2eb63960b2fe0313b948f4d526788
Walaa <Walaa.matar20@gmail.com>
2024-09-26 17:21:48 +0300

    update

--- Testing2/ContentView.swift
+++ Testing2/ContentView.swift
@@ -10,10 +10,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("Water tracker💦")
+                .font(.title2)
+                .fontWeight(.bold)
+                .font(.system(size: 20, weight: .light, design: .serif))
+            
+            Toggle(isOn: .constant(false)/*@END_MENU_TOKEN@*/) {
+                Text("Apple Health")
+                
+            }
+            .padding()
+            Stepper(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(4)/*@END_MENU_TOKEN@*/, in: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Range@*/1...10/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label@*/Text("Cups to drink per day 0")/*@END_MENU_TOKEN@*/
+            }
+            .padding()
+            
+            Button(/*@START_MENU_TOKEN@*/"Continue"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+
         }
         .padding()
     }
