b32dd874f34a844c3f77341501e25bb152052427
Walaa <Walaa.matar20@gmail.com>
2024-09-26 16:51:05 +0300

    Initial Commit

---
+++ Testing2/Testing2App.swift
@@ -0,0 +1,17 @@
+//
+//  Testing2App.swift
+//  Testing2
+//
+//  Created by Walaa on 26/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Testing2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Testing2Tests/Testing2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Testing2Tests.swift
+//  Testing2Tests
+//
+//  Created by Walaa on 26/09/2024.
+//
+
+import Testing
+@testable import Testing2
+
+struct Testing2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
